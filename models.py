b2eba45f1ff9d6d0ecb983843cfaaef5aa00f6ac
Andrew Karanja Njiyo <karanjaandrew2000@outlook.com>
2025-02-02 14:20:35 +0300

    Models:setup the pickup request model

--- models.py
+++ models.py
@@ -16,3 +16,17 @@ class User(db.Model):
 
     def __repr__(self):
         return f"<User {self.id} username={self.username} role={self.role}>"
+    
+class PickupRequest(db.Model):
+    __tablename__ = 'pickup_requests'
+
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String, nullable=False)
+    location = db.Column(db.String, nullable=False)
+    # Link each PickupRequest to the resident (User) who created it.
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    # A PickupRequest can have multiple Assignments.
+    assignments = db.relationship('Assignment', backref='pickup_request', lazy=True)
+
+    def __repr__(self):
+        return f"<PickupRequest {self.id} user_id={self.user_id}>"
